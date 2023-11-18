b0601bde603aaa9c0dded8396c42541d90dccc11
erikamagnaye <21-31350@g.batstate-u.edu.ph>
2023-11-18 18:55:36 +0800

    header change

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -12,8 +12,21 @@
             <a href="index.php" class="logo"><b>BatStateU-Lipa Grievance Management System</b></a>
                 
             <div class="top-menu">
-            	<ul class="nav pull-right top-menu">
-                    <li><a class="logout" href="logout.php">Logout</a></li>
-            	</ul>
+            <ul class="nav pull-right">
+					<!--	<li><a href="#">
+							Admin
+						</a></li>  -->
+						<li class="nav-user dropdown"style="margin-right: 100px;">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+								<img src="assets/img/ad.png" class="nav-avatar" style="height: 45px;" />
+								<b class="caret"></b>
+							</a>
+							<ul class="dropdown-menu">
+								<li><a href="change-password.php">Change Password</a></li>
+								<li class="divider"></li>
+								<li><a href="logout.php">Logout</a></li>
+							</ul>
+						</li>
+					</ul>
             </div>
         </header>
