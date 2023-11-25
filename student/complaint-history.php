5479bc8e32213de1e6761b131c38489369b49761
JennyMaeGabuya <jennymaegabuya8@gmail.com>
2023-11-26 03:09:03 +0800

    student grievance history

--- student/complaint-history.php
+++ student/complaint-history.php
@@ -18,16 +18,24 @@ if (strlen($_SESSION['login']) == 0) {
 
     <title>STUDENT | Grievance History</title>
 
+    <link type="text/css" href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link type="text/css" href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
+    <link type="text/css" href="css/theme.css" rel="stylesheet">
+    <link type="text/css" href="images/icons/css/font-awesome.css" rel="stylesheet">
+    <link type="text/css" href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600' rel='stylesheet'>
     <!-- Bootstrap core CSS -->
     <link href="assets/css/bootstrap.css" rel="stylesheet">
     <!--external css-->
     <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
+    <link rel="stylesheet" type="text/css" href="assets/css/zabuto_calendar.css">
+    <link rel="stylesheet" type="text/css" href="assets/js/gritter/css/jquery.gritter.css" />
+    <link rel="stylesheet" type="text/css" href="assets/lineicons/style.css">
 
     <!-- Custom styles for this template -->
     <link href="assets/css/style.css" rel="stylesheet">
     <link href="assets/css/style-responsive.css" rel="stylesheet">
 
-    <link href="assets/css/table-responsive.css" rel="stylesheet">
+    <script src="assets/js/chart-master/Chart.js"></script>
 
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
@@ -108,10 +116,10 @@ if (strlen($_SESSION['login']) == 0) {
                     </thead>
                     <tbody>
 
-                      <?php 
+                      <?php
                       $statusProcess = "In Process";
                       $statusClosed = "Closed";
-                         $query = mysqli_query($bd, "SELECT * FROM tablecomplaints WHERE `sr-code` = '" . $_SESSION['id'] . "' ORDER BY complaintNumber DESC");
+                      $query = mysqli_query($bd, "SELECT * FROM tablecomplaints WHERE `sr-code` = '" . $_SESSION['id'] . "' ORDER BY complaintNumber DESC");
 
                       while ($row = mysqli_fetch_array($query)) {
                       ?>
@@ -125,15 +133,24 @@ if (strlen($_SESSION['login']) == 0) {
                           <td align="center"><?php
                                               $status = $row['status'];
                                               if ($status == "" or $status == "NULL") { ?>
-                              <button type="button" class="btn btn-theme04">Not Process Yet</button>
+                              <button type="button" class="btn btn-theme04">
+                                <i class="fa fa-hourglass-start" style="font-size:16px;color:black;"></i> Not Process Yet
+                              </button>
+
                             <?php }
                                               if ($status == $statusProcess) { ?>
-                              <button type="button" class="btn btn-warning">In Process</button>
+                              <button type="button" class="btn btn-warning">
+                                <i class="fa fa-spinner fa-spin" style="font-size:16px;"></i> In Process
+                              </button>
+
                             <?php }
                                               if ($status == $statusClosed) {
                             ?>
-                              <button type="button" class="btn btn-success">Closed</button>
+                              <button type="button" class="btn btn-success">
+                                <i class="fa fa-gavel" style="font-size:16px;color:white;"></i> Closed
+                              </button>
                             <?php } ?>
+
                           <td align="center">
                             <a href="complaint-details.php?cid=<?php echo htmlentities($row['complaintNumber']); ?>">
 
@@ -153,7 +170,7 @@ if (strlen($_SESSION['login']) == 0) {
                                   }, 3000); // 3 seconds delay
                                 }
                               </script>
-                              
+
                               <button onclick="viewDetails()" class="btn btn-primary">View Details</button>
                             </a>
                           </td>
@@ -171,22 +188,42 @@ if (strlen($_SESSION['login']) == 0) {
         <! --/wrapper -->
       </section><!-- /MAIN CONTENT -->
       <?php include("includes/footer.php"); ?>
-    </section>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-    <!-- js placed at the end of the document so the pages load faster -->
-    <script src="assets/js/jquery.js"></script>
-    <script src="assets/js/bootstrap.min.js"></script>
-    <script class="include" type="text/javascript" src="assets/js/jquery.dcjqaccordion.2.7.js"></script>
-    <script src="assets/js/jquery.scrollTo.min.js"></script>
-    <script src="assets/js/jquery.nicescroll.js" type="text/javascript"></script>
+      <script src="scripts/jquery-1.9.1.min.js" type="text/javascript"></script>
+      <script src="scripts/jquery-ui-1.10.1.custom.min.js" type="text/javascript"></script>
+      <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+      <script src="scripts/flot/jquery.flot.js" type="text/javascript"></script>
+      <script src="scripts/datatables/jquery.dataTables.js"></script>
+      <script>
+        $(document).ready(function() {
+          $('.datatable-1').dataTable();
+          $('.dataTables_paginate').addClass("btn-group datatable-pagination");
+          $('.dataTables_paginate > a').wrapInner('<span />');
+          $('.dataTables_paginate > a:first-child').append('<i class="icon-chevron-left shaded"></i>');
+          $('.dataTables_paginate > a:last-child').append('<i class="icon-chevron-right shaded"></i>');
+        });
+      </script>
 
+      <!-- js placed at the end of the document so the pages load faster -->
+      <script src="assets/js/jquery.js"></script>
+      <script src="assets/js/jquery-1.8.3.min.js"></script>
+      <script src="assets/js/bootstrap.min.js"></script>
+      <script class="include" type="text/javascript" src="assets/js/jquery.dcjqaccordion.2.7.js"></script>
+      <script src="assets/js/jquery.scrollTo.min.js"></script>
+      <script src="assets/js/jquery.nicescroll.js" type="text/javascript"></script>
+      <script src="assets/js/jquery.sparkline.js"></script>
+      <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
 
-    <!--common script for all pages-->
-    <script src="assets/js/common-scripts.js"></script>
+      <!--common script for all pages-->
+      <script src="assets/js/common-scripts.js"></script>
 
-    <!--script for this page-->
+      <script type="text/javascript" src="assets/js/gritter/js/jquery.gritter.js"></script>
+      <script type="text/javascript" src="assets/js/gritter-conf.js"></script>
 
-
+      <!--script for this page-->
+      <script src="assets/js/sparkline-chart.js"></script>
+      <script src="assets/js/zabuto_calendar.js"></script>
   </body>
 
   </html>
