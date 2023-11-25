fb8ddf42cf5e765a4f63a3f2c5182f2a71e76728
JennyMaeGabuya <jennymaegabuya8@gmail.com>
2023-11-25 18:20:40 +0800

    document format

--- employee/change-password.php
+++ employee/change-password.php
@@ -44,7 +44,7 @@ if (strlen($_SESSION['login']) == 0) {
         <meta name="author" content="Dashboard">
         <meta name="keyword" content="Dashboard, Bootstrap, Admin, Template, Theme, Responsive, Fluid, Retina">
 
-        <title>Employee | Change Password</title>
+        <title>EMPLOYEE | Change Password</title>
 
         <!-- Bootstrap core CSS -->
         <link href="assets/css/bootstrap.css" rel="stylesheet">
