5710f12c2e7177f5bc362af8e46a6bef3ce619de
JennyMaeGabuya <jennymaegabuya8@gmail.com>
2023-11-26 20:01:32 +0800

    .

--- employee/index.php
+++ employee/index.php
@@ -144,7 +144,7 @@
 	<!-- You can use an image of whatever size. This script will stretch to fit in any screen size.-->
 	<script type="text/javascript" src="assets/js/jquery.backstretch.min.js"></script>
 	<script>
-		$.backstretch("assets/img/comp1.jpg", {
+		$.backstretch("assets/img/Frame.jpg", {
 			speed: 500
 		});
 	</script>
